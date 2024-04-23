2ca3213f10cc69fbcdfaf30e2600088d2e666868
d.deOliveira <deo9674@gmail.com>
2024-04-23 15:34:08 +0200

    esercizio svolto insieme a Giuseppe Favara

---
+++ src/SetElements.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+public class SetElements {
+    public static HashSet<String> getFilledHashSet() {
+        HashSet<String> set = new HashSet<String>();
+
+        // Aggiungiamo alcuni elementi all'HashSet
+        set.add("Elemento 1");
+        set.add("Elemento 2");
+        set.add("Elemento 3");
+
+        return set;
+    }
+}
+

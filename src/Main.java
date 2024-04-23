9994b7cdcb2776581a0573010c2f414f23bcd93a
d.deOliveira <deo9674@gmail.com>
2024-04-23 15:55:28 +0200

    correggo Main...

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.HashSet;
+
+//ESERCIZIO SVOLTO CON GIUSEPPE FAVARA.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                // Chiamiamo la funzione per ottenere un HashSet riempito
+                HashSet<String> set = SetElements.getFilledHashSet( );
+
+                // Creiamo un oggetto dello stesso tipo inserito nell'HashSet e popoliamolo
+                String newElement = "Elemento 3";
+
+                // Verifichiamo se l'elemento è parte del Set e stampiamo il risultato
+                Boolean presentElement = set.contains(newElement);
+                if(presentElement){
+                    System.out.println( newElement + " e' presente nel set." );
+                }else{
+                    System.out.println( newElement + " non e' presente nel set." );
+                }
+
+            }
+
         }
-    }
-}
+
